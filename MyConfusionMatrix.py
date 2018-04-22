d284e00f24b8dd9343cb66ea23f14148b75648bf
JunD-UF <32112403+JunD-UF@users.noreply.github.com>
2018-04-22 12:19:07 -0400

    Delete MyConfusionMatrix.py

--- MyConfusionMatrix.py
+++
@@ -1,9 +0,0 @@
-from sklearn import metrics
-import numpy as np
-confusion_matrix=metrics.confusion_matrix(Y,ClassNames)
-overall_accuracy=metrics.accuracy_score(Y,ClassNames)
-acc_each_class=metrics.precision_score(Y,ClassNames,average=None)
-average_accuracy=np.mean(acc_each_class)
-print('Confusion matrix: \n',confusion_matrix)
-print('Average accuracy: \n',average_accuracy)
-
